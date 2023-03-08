66e5dbca405653e8465be5ca8bf0181fc1861271
Trossty <egementopcu0@gmail.com>
2023-03-08 10:27:18 +0300

    gay

--- src/main/java/woolwars/woolwars/command/MainCommand.java
+++ src/main/java/woolwars/woolwars/command/MainCommand.java
@@ -6,7 +6,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import woolwars.woolwars.WoolWarsPlugin;
+import woolwars.woolwars.game.Game;
 import woolwars.woolwars.objects.guis.PlayGUI;
+import woolwars.woolwars.utils.Colorize;
 
 public class MainCommand implements CommandExecutor {
 
@@ -27,6 +29,43 @@ public class MainCommand implements CommandExecutor {
             getPlugin().getGUIAPI().openGUI(player,new PlayGUI(getPlugin()));
 
             return false;
+        }
+
+        if(args[0].equalsIgnoreCase("admin")){
+
+            if(args[1].equalsIgnoreCase("set")){
+
+                if(args[2].equalsIgnoreCase("location")){
+
+                    Game game = getPlugin().getGameManager().findGame(player);
+
+                    if(game == null){
+                        Colorize.sendMessage(player,"&cYou should be in game!");
+                        return false;
+                    }
+
+                    if(game.getMap().isLocationsReady()){
+                        Colorize.sendMessage(player,"&cLocations are already set!");
+                        return false;
+                    }
+
+                    switch (args[3]){
+
+                        case "RedSpawn":
+                            break;
+                        case "BlueSpawn":
+                            break;
+                        case "Center":
+                            break;
+                        case "Upgrades":
+                            break;
+
+                    }
+
+                }
+
+            }
+
         }
 
         return false;
